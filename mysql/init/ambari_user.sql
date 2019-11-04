d7b5259eddf9a598734872949c71fead6acf8a2e
Rohith Mark Varghese <marko.varghese@gmail.com>
2019-11-04 10:11:05 -0600

    fixes to work on windows docker too

--- mysql/init/ambari_user.sql
+++ mysql/init/ambari_user.sql
@@ -2,6 +2,6 @@ CREATE USER 'ambari'@'%' IDENTIFIED BY 'bigdata';
 GRANT ALL PRIVILEGES ON *.* TO 'ambari'@'%';
 CREATE USER 'ambari'@'localhost' IDENTIFIED BY 'bigdata';
 GRANT ALL PRIVILEGES ON *.* TO 'ambari'@'localhost';
-CREATE USER 'ambari'@'ambari_server' IDENTIFIED BY 'bigdata';
-GRANT ALL PRIVILEGES ON *.* TO 'ambari'@'ambari_server';
+CREATE USER 'ambari'@'ambariserver' IDENTIFIED BY 'bigdata';
+GRANT ALL PRIVILEGES ON *.* TO 'ambari'@'ambariserver';
 FLUSH PRIVILEGES;
